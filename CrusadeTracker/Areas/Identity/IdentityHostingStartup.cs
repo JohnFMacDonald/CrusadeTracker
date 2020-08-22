5cc6c6348ad03076d168c9c2ad2421833ed81ad6
JohnFMacDonald <JohnFMacDonald@users.noreply.github.com>
2020-08-22 20:56:53 -0300

    Initial Commit

---
+++ CrusadeTracker/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CrusadeTracker.Areas.Identity.Data;
+using CrusadeTracker.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CrusadeTracker.Areas.Identity.IdentityHostingStartup))]
+namespace CrusadeTracker.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedEmail = false)
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
